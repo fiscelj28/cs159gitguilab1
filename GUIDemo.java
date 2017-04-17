cc0d7e0dcdbcd4c9450b28815ab7e0b726bff9fc
Fiscella <fisceljd@l14364.cs.jmu.edu>
2017-04-17 10:59:27 -0400

    added GUIDemo.java into my dirst project

--- GUIDemo.java
+++ GUIDemo.java
@@ -12,62 +12,69 @@ import javax.swing.*;
  */
 public class GUIDemo extends JFrame
 {
-    private JPanel panel;
-    private JButton biggerButton;
-    private JButton smallerButton;
+	private JPanel panel;
+	private JButton biggerButton;
+	private JButton smallerButton;
 
-    /**
-     * Set up the application.
-     */
-    public GUIDemo()
-    {
-        setTitle("Bigger/Smaller");
-        setSize(200, 100);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel = new JPanel();
-        biggerButton = new JButton("BIGGER");
-        smallerButton = new JButton("SMALLER");
-        biggerButton.addActionListener(new ButtonHandler());
-        smallerButton.addActionListener(new ButtonHandler());
+	/**
+	 * Set up the application.
+	 */
+	public GUIDemo()
+	{
+		setTitle("Bigger/Smaller");
+		setSize(200, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new JPanel();
+		biggerButton = new JButton("BIGGER");
+		smallerButton = new JButton("SMALLER");
+		biggerButton.addActionListener(new ButtonHandler());
+		smallerButton.addActionListener(new ButtonHandler());
+		add(panel);
+		panel.add(biggerButton);
+		panel.add(smallerButton);
+		setVisible(true);
 
-        add(panel);
-        panel.add(biggerButton);
-        panel.add(smallerButton);
+		setTitle("Bigger/Smaller");
+		setSize(200, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new JPanel();
+		biggerButton = new JButton("BIGGER");
+		smallerButton = new JButton("SMALLER");
+		biggerButton.addActionListener(new ButtonHandler());
+		smallerButton.addActionListener(new ButtonHandler());
 
-        setVisible(true);
-    }
+		add(panel);
+		panel.add(biggerButton);
+		panel.add(smallerButton);
 
-    /**
-     * This inner class exists to handle button events. There are other ways
-     * this could have been done:
-     * 
-     * 1. GUIDemo could implement ActionListener itself. 
-     * 2. Anonymous inner classes could be used to hand the events.
-     */
-    private class ButtonHandler implements ActionListener
-    {
+		setVisible(true);
+	}
 
-        public void actionPerformed(ActionEvent e)
-        {
-            Dimension size = getSize();
+	/**
+	 * This inner class exists to handle button events. There are other ways
+	 * this could have been done:
+	 * 
+	 * 1. GUIDemo could implement ActionListener itself. 2. Anonymous inner
+	 * classes could be used to hand the events.
+	 */
+	private class ButtonHandler implements ActionListener {
 
-            if (e.getSource().equals(biggerButton))
-            {
-                setSize(size.width + 10, size.height + 10);
-            }
-            else
-            {
-                setSize(size.width - 10, size.height - 10);
-            }
+		public void actionPerformed(ActionEvent e) {
+			Dimension size = getSize();
 
-        }
-    }
+			if (e.getSource().equals(biggerButton)) {
+				setSize(size.width + 10, size.height + 10);
+			} else {
+				setSize(size.width - 10, size.height - 10);
+			}
 
-    /**
-     * Start the app by creating a GUIDemo object.
-     */
-    public static void main(String[] args)
-    {
-        GUIDemo app = new GUIDemo();
-    }
+		}
+	}
+
+	/**
+	 * Start the app by creating a GUIDemo object.
+	 */
+	public static void main(String[] args) {
+		GUIDemo app = new GUIDemo();
+	}
 }
